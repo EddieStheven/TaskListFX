edb7f5a66e7ecadfacd622cbbbdf545e2e1d04bc
EddieStheven <eddiesthevengith@gmail.com>
2024-10-12 08:44:37 -0300

    test

--- src/main/java/com/example/tesklistfx/HelloApplication.java
+++ src/main/java/com/example/tesklistfx/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.w3c.dom.ls.LSOutput;
 
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +22,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
